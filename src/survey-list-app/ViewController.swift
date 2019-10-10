97326548d8841761f8ceebb50e9d891f2a2b2d8d
amnmblchllng <56375864+amnmblchllng@users.noreply.github.com>
2019-10-10 05:02:20 +0200

    Init repo src with Swift 4

---
+++ src/survey-list-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  survey-list-app
+//
+//  Created by amnmblchllng on 10.10.19.
+//  Copyright © 2019 amnmblchllng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
