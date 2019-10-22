689f4560e88100a3b3f3f70df04e24d8583f74f7
amnmblchllng <56375864+amnmblchllng@users.noreply.github.com>
2019-10-23 01:43:12 +0200

    Move classes to directories

--- src/survey-list-app/ViewController/SurveyListViewController.swift
+++ src/survey-list-app/ViewController/SurveyListViewController.swift
@@ -107,7 +107,7 @@ class SurveyListViewController: UIViewController, UICollectionViewDelegate, UICo
     
     // get current cell and populate with data
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SurveyCollectionViewCell
         let row = indexPath.row
         let survey = surveys[row]
         cell.populate(survey: survey, action: { [weak self] survey in
@@ -135,54 +135,4 @@ class SurveyListViewController: UIViewController, UICollectionViewDelegate, UICo
 
 }
 
-// survey's collection view
-class CollectionViewCell: UICollectionViewCell {
-    
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
-    
-    var tappedTakeSurveyAction: ((Survey)->())?
-    var survey: Survey!
-    let gradient = CAGradientLayer()
-    
-    @IBAction func tappedTakeSurvey(_ sender: Any) {
-        tappedTakeSurveyAction?(survey)
-    }
-    
-    // add gradient overlay
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        gradient.colors = [UIColor.black.cgColor, UIColor.clear.cgColor]
-        gradient.opacity = 0.4
-        imageView.layer.addSublayer(gradient)
-    }
-    
-    // layout gradient
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        gradient.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height * 0.5)
-    }
-    
-    func populate(survey: Survey, action: @escaping (Survey)->()) {
-        // some surveys' images point to unavailable cloudfront bucket so it displays nothing
-        let imageURL = URL(string: survey.coverImageUrlLarge)!
-        self.imageView.sd_setImage(with: imageURL)
-        self.titleLabel.text = survey.title
-        self.descriptionLabel.text = survey.description
-        self.survey = survey
-        self.tappedTakeSurveyAction = action
-    }
-}
-
-// survey's action button
-class TakeSurveyButton: UIButton {
-    override open var isHighlighted: Bool {
-        didSet {
-            backgroundColor = isHighlighted
-                ? UIColor(red: 0xCA/255.0, green: 0x29/255.0, blue: 0x68/255.0, alpha: 1)
-                : UIColor(red: 0xCA/255.0, green: 0x20/255.0, blue: 0x2E/255.0, alpha: 1)
-        }
-    }
-}
 
